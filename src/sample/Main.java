1c3fcc19aeb8df836bf630ebfdea781ed70ef626
raychen2 <ray.chen.3@stonybrook.edu>
2018-02-01 22:04:42 -0500

    Retry 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        System.out.println("Here's some stuff");
+        System.out.println("Here's some stuf");
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
