b711574bf22a44ff7a06a13fe3f25127ba8f7baf
raychen2 <ray.chen.3@stonybrook.edu>
2018-02-05 21:41:22 -0500

    Let's see if this works

--- hw1/data-vilij/src/actions/AppActions.java
+++ hw1/data-vilij/src/actions/AppActions.java
@@ -5,6 +5,7 @@ import vilij.templates.ApplicationTemplate;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import javafx.application.Platform;
 
 /**
  * This is the concrete implementation of the action handlers required by the application.
@@ -26,6 +27,7 @@ public final class AppActions implements ActionComponent {
     @Override
     public void handleNewRequest() {
         // TODO for homework 1
+
     }
 
     @Override
@@ -41,6 +43,7 @@ public final class AppActions implements ActionComponent {
     @Override
     public void handleExitRequest() {
         // TODO for homework 1
+        Platform.exit();
     }
 
     @Override
--- hw1/data-vilij/src/ui/AppUI.java
+++ hw1/data-vilij/src/ui/AppUI.java
@@ -1,12 +1,22 @@
 package ui;
 
 import actions.AppActions;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import vilij.propertymanager.PropertyManager;
 import vilij.templates.ApplicationTemplate;
 import vilij.templates.UITemplate;
+
+import static vilij.settings.PropertyTypes.GUI_RESOURCE_PATH;
+import static vilij.settings.PropertyTypes.ICONS_RESOURCE_PATH;
+import static settings.AppPropertyTypes.*;
 
 /**
  * This is the application's user interface implementation.
@@ -19,27 +29,50 @@ public final class AppUI extends UITemplate {
     ApplicationTemplate applicationTemplate;
 
     @SuppressWarnings("FieldCanBeLocal")
+    private Pane                         dataSpace;      // the half of the workspace devoted to data
+    private Pane                         graphSpace;     // the other half for the graph
     private Button                       scrnshotButton; // toolbar button to take a screenshot of the data
     private ScatterChart<Number, Number> chart;          // the chart where data will be displayed
     private Button                       displayButton;  // workspace button to display data on the chart
     private TextArea                     textArea;       // text area for new data input
     private boolean                      hasNewText;     // whether or not the text area has any new data since last display
+    private Label                        displayTitle;   // label for title
+    private NumberAxis xAxis;
+    private NumberAxis yAxis;
+
+    private static final String SEPARATOR = "/";
+    private static final String TEXT_TITLE = "Data File";
+    private static final String DISPLAY = "Display";
+    private static final String DATA_VISUALIZATION = "Data Visualization";
+    private String ssPath;
 
     public ScatterChart<Number, Number> getChart() { return chart; }
 
     public AppUI(Stage primaryStage, ApplicationTemplate applicationTemplate) {
         super(primaryStage, applicationTemplate);
         this.applicationTemplate = applicationTemplate;
+        workspace = new HBox();
+        xAxis = new NumberAxis();
+        yAxis = new NumberAxis();
     }
 
     @Override
     protected void setResourcePaths(ApplicationTemplate applicationTemplate) {
         super.setResourcePaths(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        String iconsPath = SEPARATOR + String.join(SEPARATOR,
+                manager.getPropertyValue(GUI_RESOURCE_PATH.name()),
+                manager.getPropertyValue(ICONS_RESOURCE_PATH.name()));
+        ssPath = String.join(SEPARATOR, iconsPath, manager.getPropertyValue(SCREENSHOT_ICON.name()));
     }
 
     @Override
     protected void setToolBar(ApplicationTemplate applicationTemplate) {
         // TODO for homework 1
+        super.setToolBar(applicationTemplate);
+        PropertyManager manager = applicationTemplate.manager;
+        scrnshotButton = super.setToolbarButton(ssPath, manager.getPropertyValue(SCREENSHOT_TOOLTIP.name()), true);
+        toolBar.getItems().add(scrnshotButton);
     }
 
     @Override
@@ -60,14 +93,32 @@ public final class AppUI extends UITemplate {
 
     @Override
     public void clear() {
-        // TODO for homework 1
+        // TODO for homework 1 i think clears the chart?
     }
 
     private void layout() {
-        // TODO for homework 1
+        // TODO for homework 1 i think does the layout of the charts etc
+        displayTitle = new Label(TEXT_TITLE);
+        textArea = new TextArea();
+        displayButton = new Button(DISPLAY);
+        dataSpace = new VBox(displayTitle, textArea, displayButton);
+        chart = new ScatterChart<>(xAxis, yAxis);
+        chart.setTitle(DATA_VISUALIZATION);
+        chart.setPrefSize(windowWidth*0.9, windowHeight*0.66);
+        graphSpace = new VBox(chart);
+        workspace.getChildren().addAll(dataSpace, chart);
+        appPane.getChildren().add(workspace);
     }
 
     private void setWorkspaceActions() {
         // TODO for homework 1
+        // must make the new button only enable when there is text in the textarea and disable when there isnt
+        // same with save?
+        textArea.textProperty().addListener(e -> {
+            newButton.setDisable(false);
+            saveButton.setDisable(false);
+        });
+        displayButton.setOnAction(e -> System.out.println(textArea.getText()));
+
     }
 }
